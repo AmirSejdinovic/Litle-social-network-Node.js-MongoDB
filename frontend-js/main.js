e0ad64ae886f357f019dfcdb71846d6572f0875c
Amir Sejdinovic <amir24V@yahoo.com>
2020-01-17 14:45:19 +0100

    Creating sendinq server req for live search with axios

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,4 +1,9 @@
 //Importing the search module from modules file
 import Search from './modules/search';
-//Creating new instance of Search class
-new Search();
+
+//Only if this element exists on DOM create new instace of the class. 
+if(document.querySelector(".header-search-icon")){
+  //Creating new instance of Search class.This trigar our constructor from class and set everything in motion
+  new Search();
+}
+
--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,3 +1,6 @@
+//Importing the axios package
+import axios from 'axios';
+
 //JS class new sintax because it I use babel to render this code on valid JS for all type of browsers
 export default class Search{
   //1. Create propreties to select DOM elements and keep track of any useful data
@@ -9,6 +12,16 @@ export default class Search{
     this.overlay = document.querySelector('.search-overlay');
     //Selecting closing icon
     this.closeIcon = document.querySelector(".close-live-search");
+    //Selecting the input field
+    this.inputField = document.querySelector("#live-search-field");
+    //Selecting the container for displaying results
+    this.resulrsArea = document.querySelector(".live-search-results");
+    //Selecting loader element
+    this.loaderIcon = document.querySelector(".circle-loader");
+    //Create typinwaitTimer
+    this.typingWaitTimer;
+    //Creating proprety of previus value
+    this.previusValue = "";
     //Caling the events method in the constructor
     this.events();
 
@@ -17,6 +30,11 @@ export default class Search{
   //2. Events 
   //Creating events method through class 
   events(){
+    //Event listener for loader key up on input
+    this.inputField.addEventListener("keyup", ()=>{
+      //Caling the keypress method 
+       this.keyPressHandler();
+    })
     this.closeIcon.addEventListener("click", ()=>{
       //Calling the method of closeOverlay
         this.closeOverlay();
@@ -30,10 +48,52 @@ export default class Search{
   }
 
   //3. Methods
+  //Creating method for loader etc when someone cliks on input filed than start the loader
+  keyPressHandler(){
+    //Creating the variable value and asiginig it the current value of input field
+    let value = this.inputField.value;
+
+    //If the value is not empy and is not empy string etc previus value, than do this code
+    if(value != "" && value != this.previusValue){
+      //Reseting timer
+      clearTimeout(this.typingWaitTimer);
+      //Calling the showloader icon method
+       this.showLoaderIcon();
+       this.typingWaitTimer = setTimeout(()=>{
+         //Calling the method sencRequest
+          this.sendRequest();
+       },3000);
+
+    }
+
+
+    //Upadting the previus value with value of input field
+    this.previusValue = value;
+  }
+   //Method for send requests
+  sendRequest(){
+    //Calling the axios and adding the post method inside this method i provide the first the url than the second parametar is the value which will bee send via this request in my case it is the current value of input field
+    //Because axios is return the promes i added then and catch method
+    axios.post('/search', {searchTerm: this.inputField.value}).then(()=>{
+
+    }).catch(()=>{
+       alert("Hello the reuqest failed");
+    });
+  }
+  //Creating the show loader method
+  showLoaderIcon(){
+    this.loaderIcon.classList.add("circle-loader--visible");
+  }
   //Creating method inside the class. This method will calle above in the eventlistener and this method will open the overlay for search 
   opentOverlay(){
     //Adding the visilbe calss to overlay div
     this.overlay.classList.add("search-overlay--visible");
+     //Calling the setTimeout method which will wait to 50 ms and than will do the code. This is because it will have an issue on old browsers if I do not put it in the timeout method
+     setTimeout(()=>{
+       //This will focuse the inputField or another words it will place user cursor so it becaome to type.
+    this.inputField.focus();
+     },50);
+    
   }
   //Method for closing the overlay
   closeOverlay(){
@@ -56,7 +116,7 @@ export default class Search{
     <div class="search-overlay-bottom">
       <div class="container container--narrow py-3">
         <div class="circle-loader"></div>
-        <div class="live-search-results live-search-results--visible">
+        <div class="live-search-results">
           <div class="list-group shadow-sm">
             <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
 
