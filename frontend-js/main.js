3f2d9517a19561512b6178780719f8e5759e9a8b
Amir Sejdinovic <amir24V@yahoo.com>
2020-01-31 10:47:00 +0100

    Form validation front end

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -2,6 +2,13 @@
 import Search from './modules/search';
 //Importing chat calsss
 import Chat from './modules/chat';
+//Importing registration from from the modules folder
+import RegistrationForm from './modules/registrationFrom';
+//Run this code only if on don has registrtion form
+if(document.querySelector("#registration-form")){
+     //Creating the new instac of class
+     new RegistrationForm();
+}
 //Only if this element egzists on the dom than do this code in if block
 if(document.querySelector("#chat-wrapper")){
     //Creating the new instance, thos will triget the consturctor and will passe all config from it
