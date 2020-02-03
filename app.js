d8ef4d25ac028686010437b7f08551ccbbb3ed83
Amir Sejdinovic <amir24V@yahoo.com>
2020-02-03 13:46:26 +0100

    CSRF security fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Here I storing in the variable express. After this I will use this variable to call epxress
 const express = require('express');
+const csrf = require('csurf');
 //Importing the session npm package 
 const session = require('express-session');
 //Importing the connect-mongo npm package and referece to the session package above
@@ -70,8 +71,30 @@ app.use(express.static('public'))
 app.set('views', 'views');
 //With this we tell expess which template engine we'll be use for rednering. Here I will be use the ejs template engine. To working like it should we must install the ejs package from npm. We instal it with this line of code in terminal npm install ejs
 app.set('view engine', 'ejs');
+
+app.use(csrf());
+
+//creating midlewere
+app.use(function(req,res,next){
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
 //Here I call method of use and pased two arguments first is the url for which I will use the router and the second is the variable which contains router cll
 app.use('/',router);
+
+app.use(function(err, req, res, next){
+  if(err){
+     if(err.code == "EBADCSRFTOKEN"){
+        req.flash('errors', "Cross site request forgery detected")
+        req.session.save(()=>{
+          res.redirect('/')
+        })
+     }else{
+       res.render("404");
+
+     }
+  }
+})
 
 const server = require('http').createServer(app);
 
